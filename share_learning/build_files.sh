174fc5ee84478c7e408a1b90204759c20405036f
Krishna Rimal <genuinetechnology9@gmail.com>
2023-02-09 22:14:58 +0545

    Added vercel configs

--- share_learning/build_files.sh
+++ share_learning/build_files.sh
@@ -1,7 +1,12 @@
 
 echo "BUILD START"
 
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "MAKE MIGRATIONS"
+
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "BUILD END"
